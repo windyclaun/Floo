15339b5b697afc8a5a540a2a179f2b5b359330fc
Windy <windynapitupulu3108@gmail.com>
2026-08-21 10:28:55 +0700

    First Coding SetUp

---
+++ Floo/Floo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Floo
+//
+//  Created by Windy Claudia Napitupulu on 21/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Floo/Floo/FlooApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlooApp.swift
+//  Floo
+//
+//  Created by Windy Claudia Napitupulu on 21/08/26.
+//
+
+import SwiftUI
+
+@main
+struct FlooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
